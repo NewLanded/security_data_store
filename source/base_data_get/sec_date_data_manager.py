917405eb2cab20f6056c2265d443ed1be4b3b7da
flydedog <l1141041@163.com>
2018-05-27 12:20:47 +0800

    nothing

---
+++ source/base_data_get/sec_date_data_manager.py
@@ -0,0 +1,22 @@
+import datetime
+
+from source.moudle.base_info_moudle import Sec_Date_Info
+from source.util.date_util import get_date_range
+from source.util.db_util import get_connection
+
+
+def start():
+    session = get_connection()
+    start_date = datetime.datetime(2016, 1, 1)
+    end_date = datetime.datetime(2018, 12, 31)
+
+    date_range = get_date_range(start_date, end_date)
+    for date in date_range:
+        is_workday_flag = 1 if datetime.datetime.isoweekday(date) <= 5 else 0
+        new_data = Sec_Date_Info(date=date, is_workday_flag=is_workday_flag)
+        session.add(new_data)
+        session.commit()
+
+
+if __name__ == "__main__":
+    start()
--- source/moudle/base_info_moudle.py
+++ source/moudle/base_info_moudle.py
@@ -8,19 +8,28 @@ class S_Info(Base):
     __tablename__ = 's_info'
 
     id = Column(Integer, primary_key=True)
-    code = Column(String(6), nullable=False)
-    name = Column(String(100), nullable=False)
-    hs300_weight = Column(Float, nullable=False)
-    data_date = Column(DATETIME, nullable=False)
-    update_date = Column(DATETIME, nullable=True)
+    code = Column(String(6), nullable=True, default=None)
+    name = Column(String(100), nullable=True, default=None)
+    hs300_weight = Column(Float, nullable=True, default=None)
+    data_date = Column(DATETIME, nullable=True, default=None)
+    update_date = Column(DATETIME, nullable=True, default=None)
 
 
 class Failed_Code(Base):
     __tablename__ = 'failed_code'
 
     id = Column(Integer, primary_key=True)
-    code = Column(String(6), nullable=False)
-    index = Column(String(6), nullable=False)
-    error_message = Column(String(10000), nullable=False)
-    date = Column(DATETIME, nullable=False)
-    update_date = Column(DATETIME, nullable=True)
+    code = Column(String(6), nullable=True, default=None)
+    index = Column(String(6), nullable=True, default=None)
+    error_message = Column(String(10000), nullable=True, default=None)
+    date = Column(DATETIME, nullable=True, default=None)
+    update_date = Column(DATETIME, nullable=True, default=None)
+
+
+class Sec_Date_Info(Base):
+    __tablename__ = 'sec_date_info'
+
+    id = Column(Integer, primary_key=True)
+    date = Column(Integer, nullable=True, default=None)
+    is_workday_flag = Column(Integer, nullable=True, default=0)
+
