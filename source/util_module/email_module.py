4d28b407adbe2eafe751e11f5b9a55c600726c0c
NewLanded <l1141041@163.com>
2018-09-21 13:24:47 +0800

    email

--- source/util_module/email_module.py
+++ source/util_module/email_module.py
@@ -1,6 +1,6 @@
 import datetime
 import smtplib
-from email.header import Header
+from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 from source.conf import FROM_ADDR, TO_ADDR, SMTP_SERVER, PASSWORD
@@ -9,15 +9,16 @@ from source.util_base.db_util import store_failed_message, get_connection
 
 def send_email(subject, text):
     try:
-        msg = MIMEText(text, 'plain', 'utf-8')
-        msg['From'] = FROM_ADDR
-        msg['To'] = TO_ADDR
-        msg['Subject'] = Header(subject, 'utf-8').encode()
+        mail_msg = MIMEMultipart()
+        mail_msg['Subject'] = subject
+        mail_msg['From'] = FROM_ADDR
+        mail_msg['To'] = TO_ADDR
+        mail_msg.attach(MIMEText(text, 'html', 'utf-8'))
 
-        server = smtplib.SMTP_SSL(SMTP_SERVER, 465)
-        # server.set_debuglevel(1)
-        server.login(FROM_ADDR, PASSWORD)
-        server.sendmail(TO_ADDR, [TO_ADDR], msg.as_string())
+        server = smtplib.SMTP_SSL()
+        server.connect(SMTP_SERVER, 465)  # 连接smtp服务器
+        server.login(FROM_ADDR, PASSWORD)  # 登录邮箱
+        server.sendmail(FROM_ADDR, [TO_ADDR], mail_msg.as_string())  # 发送邮件
         server.quit()
     except Exception as e:
         session = get_connection()
