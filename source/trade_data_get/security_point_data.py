037c83d793e522c0d8f1fc62e7928edd5024b2b1
NewLanded <l1141041@163.com>
2018-10-08 09:29:11 +0800

    security point data get all ts_code data once

--- source/trade_data_get/security_point_data.py
+++ source/trade_data_get/security_point_data.py
@@ -4,38 +4,31 @@ import time
 import tushare as ts
 
 from source.conf import PRO_KEY
-from source.util_base.date_util import convert_datetime_to_str, get_date_range
+from source.util_base.date_util import convert_datetime_to_str
 from source.util_base.db_util import get_connection, store_failed_message
 from source.util_data.date import Date
 from source.util_data.stock import Stock
-from source.util_table_module.base_info_module import S_Info
 from source.util_table_module.data_module import Security_Point_Data
 
 ts.set_token(PRO_KEY)
 pro = ts.pro_api()
 
 
-def get_ts_codes(session):
-    ts_codes = session.query(S_Info.ts_code).all()
-    ts_codes = [i[0] for i in ts_codes]
-    return ts_codes
-
-
-def get_security_point_data(ts_code, start_date_str, end_date_str):
+def get_all_security_point_data(data_date_str):
     time.sleep(1)
-    security_point_data = pro.daily(ts_code=ts_code, start_date=start_date_str, end_date=end_date_str)
-    return security_point_data
+    all_security_point_data = pro.daily(trade_date=data_date_str)
+    time.sleep(1)
+    return all_security_point_data
 
 
 def store_security_point_data(session, security_point_data):
-    for index, data in security_point_data.iterrows():
-        new_data = Security_Point_Data(ts_code=data["ts_code"], trade_date=data["trade_date"], open=data["open"],
-                                       high=data["high"], low=data["low"],
-                                       close=data["close"], pre_close=data["pre_close"], change=data["change"],
-                                       pct_change=data["pct_change"], vol=data["vol"], amount=data["amount"],
-                                       update_date=datetime.datetime.now())
-        session.add(new_data)
-        session.commit()
+    new_data = Security_Point_Data(ts_code=security_point_data["ts_code"], trade_date=security_point_data["trade_date"], open=security_point_data["open"],
+                                   high=security_point_data["high"], low=security_point_data["low"],
+                                   close=security_point_data["close"], pre_close=security_point_data["pre_close"], change=security_point_data["change"],
+                                   pct_change=security_point_data["pct_change"], vol=security_point_data["vol"], amount=security_point_data["amount"],
+                                   update_date=datetime.datetime.now())
+    session.add(new_data)
+    session.commit()
 
 
 def start(date_now=None):
@@ -45,20 +38,17 @@ def start(date_now=None):
     session = get_connection()
 
     if Date().is_workday(date_now):
-        ts_codes = get_ts_codes(session)
-        start_date, end_date = date_now, date_now
-        for ts_code in ts_codes:
-            if Stock().is_valid_security_normal(ts_code):
-                try:
-                    security_point_data = get_security_point_data(ts_code, convert_datetime_to_str(start_date),
-                                                                  convert_datetime_to_str(end_date))
+        try:
+            all_security_point_data = get_all_security_point_data(convert_datetime_to_str(date_now))
+            for index, security_point_data in all_security_point_data.iterrows():
+                if Stock().is_valid_security_normal(security_point_data["ts_code"]):
                     store_security_point_data(session, security_point_data)
-                except Exception as e:
-                    store_failed_message(session, ts_code, "000002", str(e), None)
+        except Exception as e:
+            store_failed_message(session, "", "000002", str(e), None)
+
     session.close()
 
 
 if __name__ == "__main__":
-    start()
-    # for date in get_date_range(datetime.datetime(2018, 6, 1), datetime.datetime(2018, 6, 3)):
-    #     start(date)
+    pass
+    # start(datetime.datetime(2018, 5, 31))
